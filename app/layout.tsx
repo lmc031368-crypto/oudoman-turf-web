60693986f029442dce89a561c52825130f703525
cm l <lmc031368@gmail.com>
2026-04-13 17:27:56 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 
 export const metadata = {
-  title: "Oudoman Turf",
+  title: "Handan Oudoman Sports Technology Co., Ltd.",
+  description: "Global Manufacturer of Artificial Turf",
   icons: {
-    // 这里的 🌿 就是浏览器标签页上的可爱图标
     icon: `data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2280%22>🌿</text></svg>`,
   },
 };
